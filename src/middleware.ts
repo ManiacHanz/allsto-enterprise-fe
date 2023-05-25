4399dad2ccc1ac1de66f2fec3283f588e23b8561
ManiacHanz <182490078@qq.com>
2023-05-25 14:40:16 +0800

    feat: comment middleware

--- src/middleware.ts
+++ src/middleware.ts
@@ -5,13 +5,12 @@ import { isAuthenticated } from "./utils/auth"
 export function middleware(request: NextRequest) {
   // const allCookies = request.cookies.getAll()
   // let loginToken = request.cookies.get("next-auth.session-token")?.value
-
-  if (request.nextUrl.pathname.startsWith("/pay")) {
-    if (!isAuthenticated(request)) {
-      return new NextResponse(
-        JSON.stringify({ success: false, message: "authentication failed" }),
-        { status: 401, headers: { "content-type": "application/json" } }
-      )
-    }
-  }
+  // if (request.nextUrl.pathname.startsWith("/pay")) {
+  //   if (!isAuthenticated(request)) {
+  //     return new NextResponse(
+  //       JSON.stringify({ success: false, message: "authentication failed" }),
+  //       { status: 401, headers: { "content-type": "application/json" } }
+  //     )
+  //   }
+  // }
 }
